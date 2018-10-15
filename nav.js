cf1dd773aaaa7f8fca986e735e283c63821fb05a
yhsuan93 <hsuancelia@gmail.com>
2018-10-15 17:15:18 +0800

    addnavbar

---
+++ nav.js
@@ -0,0 +1,4 @@
+var mobileNavToggle = document.querySelector("#mobile_nav_toggle")
+mobileNavToggle.onclick = function () {
+    document.querySelector(".mobile_nav_item").classList.toggle('active');
+}
