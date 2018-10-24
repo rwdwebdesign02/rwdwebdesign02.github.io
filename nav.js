60fdd76c4272c8e10584104b0b5813f1fca170e9
yhsuan93 <hsuancelia@gmail.com>
2018-10-24 14:12:39 +0800

    nav 展開狀態點空白處關閉

--- nav.js
+++ nav.js
@@ -2,3 +2,9 @@ var mobileNavToggle = document.querySelector("#mobile_nav_toggle")
 mobileNavToggle.onclick = function () {
     document.querySelector(".mobile_nav_item").classList.toggle('active');
 }
+
+
+var close_nav = document.querySelector("#close_nav")
+close_nav.onclick = function () {
+    document.querySelector(".close_nav").classList.toggle('active');
+}
